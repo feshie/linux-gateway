e950deaff310b162132fe382a029be3e6a4c814d
Philip Basford <pjb@feshie-estate.mountainsensing.org>
2014-09-17 16:07:54 +0100

    Settings for the mini-itx board

--- src/gateway_config.py
+++ src/gateway_config.py
@@ -1,12 +1,12 @@
 from logging import ERROR
 
-HOST_NAME = 'aaaa::1'
+HOST_NAME = '2a01:348:24b:2::1'
 PORT_NUMBER = 8081 # Change this!
 
 NEXT_SERVER = 'http://data.mountainsensing.org/feshie/reciever/upload.php' # The address of pthe server after the proxy. Change this too!
 
 DEFAULT_LOG_LEVEL = ERROR
 
-BASE_DIR = "."
+BASE_DIR = "/ms"
 QUEUE_DIR = "/queue"
 ARCHIVE_DIR = "/archive"
