a6436cb1881edd53658ccd1bd84c91f4b12f5130
Arthur Fabre <arthur@arthurfabre.com>
2015-06-23 16:14:14 +0100

    Initial fetcher commit
    Add build system, includes and test source file.

---
+++ fetcher/src/org/mountainsensing/fetcher/Main.java
@@ -0,0 +1,12 @@
+package org.mountainsensing.fetcher;
+
+/**
+ *
+ * @author af1g12
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Testing!");
+    }
+}
