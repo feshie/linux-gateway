3ea166ef458c5b5894daa290a718f6c7b85a9cd4
Arthur Fabre <arthur@arthurfabre.com>
2015-06-24 11:14:57 +0100

    Fetcher can now decode Samples

--- fetcher/src/org/mountainsensing/fetcher/Main.java
+++ fetcher/src/org/mountainsensing/fetcher/Main.java
@@ -1,5 +1,6 @@
 package org.mountainsensing.fetcher;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.logging.Level;
@@ -7,6 +8,7 @@ import java.util.logging.Logger;
 
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
+import org.mountainsensing.pb.Readings.Sample;
 
 /**
  *
@@ -20,22 +22,31 @@ public class Main {
         URI uri = null; // URI parameter of the request
 		
 		if (args.length > 0) {
-			
+		    System.out.println("Attempting to get sample from node " + args[0]);
+            
 			// input URI from command line arguments
 			try {
-				uri = new URI(args[0]);
+				uri = new URI("coap://[aaaa::c30c:0:0:" + args[0] + "]/sample");
 			} catch (URISyntaxException e) {
 				System.err.println("Invalid URI: " + e.getMessage());
 				System.exit(-1);
 			}
 			
 			CoapClient client = new CoapClient(uri);
-
 			CoapResponse response = client.get();
 			
-			if (response!=null) {
-				
-				System.out.println(response.getResponseText());
+			if (response != null) {
+                Sample sample = null;
+                
+                try {
+                    sample = Sample.parseFrom(response.getPayload());
+                } catch (InvalidProtocolBufferException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+				System.out.println();
+
+                System.out.println(sample);
 				
 			} else {
 				System.out.println("No response received.");
