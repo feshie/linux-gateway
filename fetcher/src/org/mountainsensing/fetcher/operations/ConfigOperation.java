a8e33af724a0d84787186b8588e8d582c810de8c
Arthur Fabre <arthur@arthurfabre.com>
2015-06-29 17:02:49 +0100

    Implement get/set config

--- fetcher/src/org/mountainsensing/fetcher/operations/ConfigOperation.java
+++ fetcher/src/org/mountainsensing/fetcher/operations/ConfigOperation.java
@@ -1,10 +1,21 @@
 package org.mountainsensing.fetcher.operations;
 
+import com.beust.jcommander.IStringConverter;
+import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
 import org.mountainsensing.fetcher.Operation;
+import org.mountainsensing.pb.Settings.SensorConfig;
 
 /**
  *
@@ -23,7 +34,20 @@ public abstract class ConfigOperation extends Operation {
 
         @Override
         public void processNode(URI uri, int timeout) throws IOException {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+			CoapClient client = new CoapClient(uri);
+            
+            log.log(Level.FINE, "Attempting to get config from: {0}", client.getURI());
+            
+            CoapResponse response = client.get();
+            
+            if (response != null && response.isSuccess()) {
+                SensorConfig config = SensorConfig.parseDelimitedFrom(new ByteArrayInputStream(response.getPayload()));
+                log.log(Level.FINER, "Config succesfully got from {0}", client.getURI());
+                System.out.println(config);
+                return;
+            }
+
+            throw new IOException("Failed to get config from: " + client.getURI());
         }
     }
 
@@ -32,10 +56,49 @@ public abstract class ConfigOperation extends Operation {
      */
     @Parameters(commandDescription = "Set the configuration of the node(s)")
     public static class Set extends ConfigOperation {
+        
+        @Parameter(names = {"-a1", "--adc1"}, description = "ADC1 should be enabled")
+        private boolean hasAdc1 = false;
+        
+        @Parameter(names = {"-a2", "--adc2"}, description = "ADC1 should be enabled")
+        private boolean hasAdc2 = false;
+
+        @Parameter(names = {"-r", "--rain"}, description = "Rain sensor is connected to node(s)")
+        private boolean hasRain = false;
+
+        @Parameter(names = {"-i", "--interval"}, description = "Sampling interval in seconds")
+        private int interval = 1200;
+
+        @Parameter(names = {"-a", "--avr"}, converter = FileConverter.class, description = "AVR(s) connected to the node(s)")
+        private List<Integer> avrs = new ArrayList<>();
+
+        public class FileConverter implements IStringConverter<Integer> {
+            @Override
+            public Integer convert(String value) {
+                return Integer.parseInt(value, 16);
+            }
+        }
 
         @Override
         public void processNode(URI uri, int timeout) throws IOException {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            SensorConfig config = SensorConfig.newBuilder().setInterval(interval).setHasADC1(hasAdc1).setHasADC2(hasAdc2).setHasRain(hasRain).addAllAvrIDs(avrs).build();
+            
+			CoapClient client = new CoapClient(uri);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            config.writeDelimitedTo(out);
+            
+            log.log(Level.FINE, "Attempting to post config to: {0}", client.getURI());
+            
+			CoapResponse response = client.post(out.toByteArray(), MediaTypeRegistry.APPLICATION_OCTET_STREAM);
+			if (response != null && response.isSuccess()) {
+                log.log(Level.FINER, "Config succesfully posted to {0}", client.getURI());
+                System.out.println("Config set to: ");
+                System.out.println(config);
+                return;
+            }
+            
+            log.log(Level.FINER, "Failed to post config to {0}", client.getURI());
+            throw new IOException("Failed to post config to: " + client.getURI());
         }
     }
 
