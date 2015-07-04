f29236b020cfd00b20ca83936b8d5b6ac127cb96
Arthur Fabre <arthur@arthurfabre.com>
2015-07-04 22:54:51 +0100

    Better error handling for grab sample
    Retries are now reset every time we succesfully get a sample.
    The CoAP response code is checked for 404 to know when we've reached the
    end of the available Samples

--- fetcher/src/org/mountainsensing/fetcher/operations/SampleOperation.java
+++ fetcher/src/org/mountainsensing/fetcher/operations/SampleOperation.java
@@ -14,9 +14,12 @@ import java.util.logging.Logger;
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.coap.CoAP.ResponseCode;
+import org.mountainsensing.fetcher.CoapException;
+import org.mountainsensing.fetcher.CoapException.Method;
 import org.mountainsensing.fetcher.utils.EpochDate;
 import org.mountainsensing.fetcher.Main;
 import org.mountainsensing.fetcher.Operation;
+import org.mountainsensing.fetcher.utils.Pair;
 import org.mountainsensing.fetcher.utils.UTCDateFormat;
 import org.mountainsensing.pb.Readings.Sample;
 import org.mountainsensing.pb.Rs485Message.Rs485;
@@ -33,24 +36,16 @@ public abstract class SampleOperation extends Operation {
      */
     private static final int LATEST_SAMPLE = 0;
 
-    /**
-     * Canary value indicating no further samples should be processed.
-     */
-    private static final int NO_MORE_SAMPLES = -1;
-
     private static final String RESSOURCE = "sample";
     
     @Parameter(names = {"-s", "--sample-id"}, description = "Sample id. " + LATEST_SAMPLE + " for latest sample.")
     private int sampleId = LATEST_SAMPLE;
 
-    @Parameter(names = {"-a", "--all"}, description = "Process all samples from the node(s). This overrides any sample-id set.")
-    private boolean shouldProcessAll = false;
-        
     @Parameters(commandDescription = "Get samples from the node(s)")
     public static class Get extends SampleOperation {
         
         @Override
-        public int processSample(URI uri) throws IOException {
+        public void processSample(URI uri) throws IOException {
             Sample sample = getSample(uri);
             log.log(Level.INFO, "Got sample: \n{0}", sampleToString(sample));
 
@@ -60,9 +55,6 @@ public abstract class SampleOperation extends Operation {
                     log.log(Level.INFO, "RS485: \n{0}", rs485);
                 }
             }
-
-            // Subtract 1 from the id to try and find a previous sample
-            return sample.getId() - 1;
         }
     }
 
@@ -70,12 +62,9 @@ public abstract class SampleOperation extends Operation {
     public static class Delete extends SampleOperation {
 
         @Override
-        public int processSample(URI uri) throws IOException {
+        public void processSample(URI uri) throws IOException {
             deleteSample(uri);
             log.log(Level.INFO, "Deleted sample: {0}", uri);
-            
-            // We don't support deleting ranges / all samples
-            return NO_MORE_SAMPLES;
         }
     }
 
@@ -85,8 +74,10 @@ public abstract class SampleOperation extends Operation {
         @Parameter(names = {"-d", "--destination"}, description = "Directory in which to output protocol buffer encoded samples")
         private String dir = "/ms/queue/";
 
-        @Override
-        public int processSample(URI uri) throws IOException {
+        @Parameter(names = {"-a", "--all"}, description = "Grab all samples from the node(s). This overrides any sample-id set.")
+        private boolean shouldProcessAll = false;
+
+        private void grabSample(URI uri) throws IOException, CoapException {
             Sample sample = getSample(uri);
 
             log.log(Level.INFO, "Got sample with id {0} from node {1}", new Object[] {sample.getId(), uri.getHost()});
@@ -102,20 +93,55 @@ public abstract class SampleOperation extends Operation {
             deleteSample(getURI(uri, sample.getId()));
             log.log(Level.INFO, "Sample {0} deleted from node", sample.getId());
             // Always just get the latest sample, seeing as we've deleted the previous one.
-            return LATEST_SAMPLE;
+        }
+
+        @Override
+        public void processSample(URI uri) throws IOException {
+            if (shouldProcessAll) {
+                int retryAttempt = 0;
+
+                // Do the right amount of retries. Retries will be reset every time we succesfully get something.
+                while (retryAttempt < Main.getOptions().getRetries()) {
+                    try {
+                        grabSample(uri);
+                        // Reset the retry attempt on success
+                        retryAttempt = 0;
+                        break;
+                    } catch (CoapException e) {
+                        // If not found, we've reached the last sample
+                        if (e.getCode() == ResponseCode.NOT_FOUND) {
+                            log.log(Level.INFO, "No more samples available");
+                            break;
+                        }
+                        log.log(Level.WARNING, e.getMessage() + ". Got CoAP response " + e.getCode() + " using " + e.getMethod() + " on " + e.getURI(), e);
+                    } catch (IOException e) {
+                        log.log(Level.WARNING, e.getMessage(), e);
+                    }
+                    retryAttempt++;
+                }
+
+            } else {
+                grabSample(uri);
+            }
         }
     }
-      
+
+    /**
+     * 
+     * @param uri
+     * @return
+     * @throws IOException 
+     */
     public Sample getSample(URI uri) throws IOException {
         CoapClient client = new CoapClient(uri);
         log.log(Level.FINE, "Attempting to get sample from: {0}", client.getURI());
-        
+
         CoapResponse response = client.get();
-        if (response != null && response.isSuccess()) {
+        if (response != null) {
             return Sample.parseDelimitedFrom(new ByteArrayInputStream(response.getPayload()));
         }
 
-        throw new IOException("Unable to get sample from " + uri); 
+        throw new CoapException(uri, Method.GET, response, "Unable to get sample");
     }
     
     public void deleteSample(URI uri) throws IOException {
@@ -128,33 +154,20 @@ public abstract class SampleOperation extends Operation {
             return; 
         }
 
-        throw new IOException("Unable to delete sample from: " + uri);
+        throw new CoapException(uri, Method.DELETE, response, "Failed to delete Sample");
     }
 
     @Override
     public void processNode(URI uri) throws IOException {
-        // If request, process all samples
-        if (shouldProcessAll) {
-            int nextSample = LATEST_SAMPLE;
-
-            while (nextSample != NO_MORE_SAMPLES) {
-                nextSample = processSample(getURI(uri, nextSample));
-            }
-        
-        // Otherwise just process the requested sample.
-        } else {
-            processSample(getURI(uri, sampleId));
-        }
+        processSample(getURI(uri, sampleId));
     }
     
     /**
      * Process a given sample. Returns true if in the case of multiple samples the ID should be decremented.
      * @param uri
-     * @param timeout
-     * @return
      * @throws IOException 
      */
-    public abstract int processSample(URI uri) throws IOException;
+    public abstract void processSample(URI uri) throws IOException;
     
     protected int getNextSample(int currentSample) {
         return currentSample - 1;
