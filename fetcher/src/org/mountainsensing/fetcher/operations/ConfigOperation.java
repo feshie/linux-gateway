8dcb3932ad067635e536e917342de46dcb7a541d
Kirk Martinez <km@ecs.soton.ac.uk>
2016-05-31 16:31:59 +0100

    author header

--- fetcher/src/org/mountainsensing/fetcher/operations/ConfigOperation.java
+++ fetcher/src/org/mountainsensing/fetcher/operations/ConfigOperation.java
@@ -1,3 +1,8 @@
+/**
+ * set config of nodes using CoAP
+ * Arthur Fabre, University of Southampton, 2015
+ * mountainsensing.org
+ */
 package org.mountainsensing.fetcher.operations;
 
 import com.beust.jcommander.IStringConverter;
