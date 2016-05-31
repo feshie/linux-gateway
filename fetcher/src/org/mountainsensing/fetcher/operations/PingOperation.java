c9ceb04b6f7b36dc70ec6cf12141434c8c34610e
Kirk Martinez <km@ecs.soton.ac.uk>
2016-05-31 16:35:53 +0100

    author header

--- fetcher/src/org/mountainsensing/fetcher/operations/PingOperation.java
+++ fetcher/src/org/mountainsensing/fetcher/operations/PingOperation.java
@@ -1,3 +1,8 @@
+/**
+ * ping a node
+ * Arthur Fabre, University of Southampton, 2015
+ * mountainsensing.org
+ */
 package org.mountainsensing.fetcher.operations;
 
 import com.beust.jcommander.Parameters;
